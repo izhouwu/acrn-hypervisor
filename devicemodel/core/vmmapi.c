izhouwu/acrn-hypervisor

commit 4e6dcc84a831099026cc1270f322279dd882dd82

hv/dm: Allow specifying lapic_pt for post launched non-RTVM
This patch allows user to specify --lapic_pt for post launched VM that
is not an RTVM.

Signed-off-by: Yifan Liu <yifan1.liu@intel.com>
Tracked-On: ACRN-9867

devicemodel/core/vmmapi.c
@@ -184,9 +184,7 @@ vm_create(const char *name, uint64_t req_buf, int *vcpu_num)
 
 	if (lapic_pt) {
 		create_vm.vm_flag |= GUEST_FLAG_LAPIC_PASSTHROUGH;
-		create_vm.vm_flag |= GUEST_FLAG_RT;
 		create_vm.vm_flag |= GUEST_FLAG_IO_COMPLETION_POLLING;
-		create_vm.vm_flag |= GUEST_FLAG_PMU_PASSTHROUGH;
 	} else {
 		create_vm.vm_flag &= (~GUEST_FLAG_LAPIC_PASSTHROUGH);
 		create_vm.vm_flag &= (~GUEST_FLAG_IO_COMPLETION_POLLING);
@@ -197,6 +195,7 @@ vm_create(const char *name, uint64_t req_buf, int *vcpu_num)
 	strncpy((char *)create_vm.name, name, strnlen(name, MAX_VM_NAME_LEN));
 
 	if (is_rtvm) {
+		create_vm.vm_flag |= GUEST_FLAG_PMU_PASSTHROUGH;
 		create_vm.vm_flag |= GUEST_FLAG_RT;
 		create_vm.vm_flag |= GUEST_FLAG_IO_COMPLETION_POLLING;
 	}

devicemodel/core/main.c
@@ -1070,10 +1070,6 @@ main(int argc, char *argv[])
 		usage(1);
         }
 
-	if (lapic_pt == true && is_rtvm == false) {
-		lapic_pt = false;
-		pr_warn("Only a Realtime VM can use local APIC pass through, '--lapic_pt' is invalid here.\n");
-	}
 	vmname = argv[0];
 
 	if (strnlen(vmname, MAX_VM_NAME_LEN) >= MAX_VM_NAME_LEN) {

hypervisor/common/hypercall.c
@@ -224,11 +224,11 @@ int32_t hcall_create_vm(struct acrn_vcpu *vcpu, struct acrn_vm *target_vm, uint6
 				}
 
 				/*
-				 * GUEST_FLAG_RT must be set if we have GUEST_FLAG_LAPIC_PASSTHROUGH
+				 * GUEST_FLAG_LAPIC_PASSTHROUGH must be set if we have GUEST_FLAG_RT
 				 * set in guest_flags
 				 */
-				if (((vm_config->guest_flags & GUEST_FLAG_LAPIC_PASSTHROUGH) != 0UL)
-						&& ((vm_config->guest_flags & GUEST_FLAG_RT) == 0UL)) {
+				if (((vm_config->guest_flags & GUEST_FLAG_RT) != 0UL)
+						&& ((vm_config->guest_flags & GUEST_FLAG_LAPIC_PASSTHROUGH) == 0UL)) {
 					pr_err("Wrong guest flags 0x%lx\n", vm_config->guest_flags);
 				} else {
 					if (create_vm(vmid, pcpu_bitmap, vm_config, &tgt_vm) == 0) {

commit 73ef80db4bcfd9a907a804b6d1d3f569a7a42915

dm: set PMU_PT flag for lapic_pt guests
PMU passthrough can be set on 'lapic_pt' VMs, not RTVMs.

Fixes: 4e6dcc84a
Tracked-On: AUTOMOTIVE-1129
Signed-off-by: Wu Zhou <wu.zhou@intel.com>

devicemodel/core/vmmapi.c
@@ -185,6 +185,7 @@ vm_create(const char *name, uint64_t req_buf, int *vcpu_num)
 	if (lapic_pt) {
 		create_vm.vm_flag |= GUEST_FLAG_LAPIC_PASSTHROUGH;
 		create_vm.vm_flag |= GUEST_FLAG_IO_COMPLETION_POLLING;
+		create_vm.vm_flag |= GUEST_FLAG_PMU_PASSTHROUGH;
 	} else {
 		create_vm.vm_flag &= (~GUEST_FLAG_LAPIC_PASSTHROUGH);
 		create_vm.vm_flag &= (~GUEST_FLAG_IO_COMPLETION_POLLING);
@@ -195,7 +196,6 @@ vm_create(const char *name, uint64_t req_buf, int *vcpu_num)
 	strncpy((char *)create_vm.name, name, strnlen(name, MAX_VM_NAME_LEN));
 
 	if (is_rtvm) {
-		create_vm.vm_flag |= GUEST_FLAG_PMU_PASSTHROUGH;
 		create_vm.vm_flag |= GUEST_FLAG_RT;
 		create_vm.vm_flag |= GUEST_FLAG_IO_COMPLETION_POLLING;
 	}