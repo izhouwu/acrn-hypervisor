e84048c2f6153010ae188b31444f2a5f4a1b0b05
Wu Zhou <wu.zhou@intel.com>
2023-07-24 15:21:02 +0800

    dm: vm_event: add vm_event support in cmd monitor
    This patch added vm_event support in command monitor, so that vm_event
    can be sent to a client (e.g., Libvirt) through the monitor.
    As the command monitor works in socket server mode, the vm_event sending
    process is designed in this way:
    1. If a client wishes to receive vm_event, it issues a
       REGISTER_VM_EVENT_CLIENT command to the monitor.
    2. Command monitor then handles the REGISTER_VM_EVENT_CLIENT command. If
       it is legitimate, the client is registered as as vm_event receiver.
       The command monitor then send a ACK to the client, and keeps the socket
       connection.
    3. When a vm_event is generated, the command monitor send it out through
       the socket connection.
    4. Only one event client is allowed.
    5. The registration is cancelled on socket disconnection.
    
    Tracked-On: #8547
    Signed-off-by: Wu Zhou <wu.zhou@intel.com>
    Reviewed-by: Jian Jun Chen <jian.jun.chen@intel.com>

--- devicemodel/core/cmd_monitor/command_handler.h
+++ devicemodel/core/cmd_monitor/command_handler.h
@@ -10,4 +10,6 @@ extern struct socket_dev *sock_server;
 int user_vm_destroy_handler(void *arg, void *command_para);
 int user_vm_blkrescan_handler(void *arg, void *command_para);
 int user_vm_jack_event_handler(void *arg, void *command_para);
+int user_vm_register_vm_event_client_handler(void *arg, void *command_para);
+
 #endif
